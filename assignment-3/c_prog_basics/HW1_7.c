97fff7659f0887dd9f601049cf3bb3abdc8b30cc
momen magdy <m@gmail.com>
2021-04-04 21:43:25 +0200

    restoring the assignment file

---
+++ assignment-3/c_prog_basics/HW1_7.c
@@ -0,0 +1,21 @@
+/*
+ * HW1_7.c
+ *
+ *  Created on: Apr 4, 2021
+ *      Author: Ahmed
+ */
+
+//#include <stdio.h>
+//void main ()
+//{
+//	int x=5, y=4;
+//
+//	x=x+y;
+//	y=x-y;
+//	x=x-y;
+//
+//	printf("%d \n %d",x,y);
+//
+//
+//
+//}
